97130a8c6f70dd2c361a17307839122c2bff47ef
trxyzng <trxyzng37@gmail.com>
2024-02-15 00:22:58 +0700

    add change-password page

--- src/app/change-password/components/change-password/change-password.component.ts
+++ src/app/change-password/components/change-password/change-password.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, booleanAttribute } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SamePasswordValidator } from 'src/app/signup/directives/validators/same-password.directive';
+import { ChangePasswordService } from '../../services/change-password.service';
+import { CheckEmailService } from 'src/app/forgot-password/services/check-email/check-email.service';
+import { Observable } from 'rxjs';
+import { ChangePassword } from '../../services/change-password';
 
 @Component({
   selector: 'app-change-password',
@@ -10,7 +14,9 @@ import { SamePasswordValidator } from 'src/app/signup/directives/validators/same
 })
 export class ChangePasswordComponent implements OnInit{
   public constructor (
-    private router: Router
+    private router: Router,
+    private changePasswordService: ChangePasswordService,
+    private checkEmailService: CheckEmailService
   ) {};
 
   public ChangePasswordForm: FormGroup = new FormGroup ({
@@ -20,7 +26,23 @@ export class ChangePasswordComponent implements OnInit{
 
   onSubmit(){
     if (this.ChangePasswordForm.status === "VALID") {
-      alert("OK");
+      const email: string = this.checkEmailService.getEmail();
+      const password: string = this.ChangePasswordForm.value.newPassword;
+      const observable: Observable<ChangePassword> = this.changePasswordService.changePassword(email, password);
+      observable.subscribe({
+        next: (obj: ChangePassword) => {
+          const i: boolean = obj.changePassword; 
+          if (String(i) === "true") {
+            alert("Change password successfully")
+          }
+          else {
+            alert("Can not change password")
+          }
+        },
+        error: (e) => {
+          alert("Error change password: " + e)
+        }
+      })
     }
     else {
       alert("Password is not the same");
---
+++ src/app/change-password/services/change-password.ts
@@ -0,0 +1,7 @@
+export class ChangePassword {
+    public changePassword: boolean;
+
+    constructor (changePassword: boolean) {
+        this.changePassword = changePassword;
+    }
+}
