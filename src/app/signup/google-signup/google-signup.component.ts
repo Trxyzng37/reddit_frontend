ea0b085e4fa2987bd69bd10ad9b7d7bea0e144b2
trxyzng <trxyzng37@gmail.com>
2024-02-21 14:50:18 +0700

    add google-signup component

---
+++ src/app/signup/google-signup/google-signup.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-google-signup',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './google-signup.component.html',
+  styleUrl: './google-signup.component.scss'
+})
+export class GoogleSignupComponent {
+
+}
