93dffb931b4d38d985cceebebddd80e5283ad64e
trxyzng <trxyzng37@gmail.com>
2024-03-21 15:11:46 +0700

    add search communities by name service

--- src/app/header_bar/component/header-bar/header-bar.component.ts
+++ src/app/header_bar/component/header-bar/header-bar.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 import { StorageService } from 'src/app/shared/storage/storage.service';
+import { Communities } from '../../service/pojo/communities';
+import { SearchCommunitiesService } from '../../service/search-communites/search-communities.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-header-bar',
@@ -8,12 +12,12 @@ import { StorageService } from 'src/app/shared/storage/storage.service';
 })
 export class HeaderBarComponent {
   public constructor(
-    private storageService: StorageService
+    private storageService: StorageService,
+    private searchCommunitiesService: SearchCommunitiesService
   ) {}
 
   public isSignIn: boolean = false;
-  public search_value: string = 'search_value';
-  public search_result: string[] = [];
+  public search_result: Communities[] = [];
   private arr: string[] = ["aaa", "Abc", "bcd", "def", "art", "avail", "aeo", "jil", "ghi abc", "valve", "ass", "anomoly", "dawn", "browser", "ack", "saw", "Ask", "Aww", "Ass"];
 
   ngOnInit() {
@@ -28,11 +32,23 @@ export class HeaderBarComponent {
   }
 
   onChange(value: string) {
-    this.search_value = value;
-    if (value === '')
+    if (value !== " " && value !== "") {
+      const observable: Observable<Communities[]> = this.searchCommunitiesService.searchCommunities(value);
+      observable.subscribe({
+        next: (response: Communities[]) => {
+          console.log(value.toUpperCase())
+          console.log(response)
+          this.search_result = response;
+        },
+        error: (e: HttpErrorResponse) => {
+          console.log("HttpServletResponse: " + e.error.message + "\n" + "ResponseEntity: " + e.error);
+        }
+      })
+    }
+    else {
       this.search_result = [];
-    else
-      this.search_result = this.arr.filter(a => a.startsWith(value));
-    this.search_result = this.search_result.slice(0, 5);
+    }
   }
+
+
 }
