d60d874670daea011b92756446bf67ccea7de456
trxyzng <trxyzng37@gmail.com>
2024-03-25 23:04:34 +0700

    change post-main to use router-outlet

--- src/app/post-main/component/post-main/post-main.component.ts
+++ src/app/post-main/component/post-main/post-main.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrl: './post-main.component.scss'
 })
 export class PostMainComponent {
-  public arr: number[] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 }
