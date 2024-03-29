4110549fd0467f0fe5d9cc9a18a8dc93e60e915f
trxyzng <trxyzng37@gmail.com>
2024-03-29 20:41:41 +0700

    add function to check if a post is in viewport

--- src/app/post-link-list/component/post-link-list/post-link-list.component.ts
+++ src/app/post-link-list/component/post-link-list/post-link-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { PostResponse } from '../../pojo/post-response';
 import { GetPostsService } from '../../service/get-posts.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-post-link-list',
@@ -10,11 +11,18 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class PostLinkListComponent {
   public constructor(
-    private getPostService: GetPostsService
-  ) {}
+    private getPostService: GetPostsService,
+    private sanitizer: DomSanitizer
+
+  ) {
+    this.trustedHtml = this.sanitizer
+    .bypassSecurityTrustHtml(this.htmlContent)
+  }
 
   public post_result: PostResponse[] = [];
-
+  trustedHtml: any;
+  htmlContent = "<h1>Html content</h1><img src='https://res.cloudinary.com/trxyzng-photo-storage/image/upload/v1711289964/test/test.jpg' style='width: 100px; height: 50px'/>";
+  
   ngOnInit() {
     this.getPostService.getAllPosts().subscribe({
       next: (response: PostResponse[]) => {
@@ -25,6 +33,9 @@ export class PostLinkListComponent {
         console.log("HttpServletResponse: " + e.error.message + "\n" + "ResponseEntity: " + e.error);
       }
     })
+  }
 
+  addPost(o: PostResponse) {
+    this.post_result.push(o);
   }
 }
