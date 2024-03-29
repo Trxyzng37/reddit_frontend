1052451e73592bb8f3b1b2b924edbd456718c5b2
trxyzng <trxyzng37@gmail.com>
2024-03-29 20:41:41 +0700

    add function to check if a post is in viewport

--- src/app/post-link/component/post-link/post-link.component.ts
+++ src/app/post-link/component/post-link/post-link.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { PostResponse } from 'src/app/post-link-list/pojo/post-response';
 
 @Component({
   selector: 'app-post-link',
@@ -17,9 +19,34 @@ export class PostLinkComponent {
   @Input() created_at: string = "";
   @Input() vote: number = 1;
   @Input() communityIcon: string = "";
+  @Input() index: number = 0;
+  @Input() arr_length: number = 0;
 
-
+  @Output() event = new EventEmitter<PostResponse>();
+    
   on_click() {
     this.router.navigate(["/id/" + this.post_id]);
+  }
+
+  onIntersection({ target, visible }: { target: Element; visible: boolean }) {
+    if (visible) {
+      console.log("Post index: "+this.index);
+      console.log("LENGTH: "+this.arr_length);
+      console.log(this.index===this.arr_length-1)
+      if(this.index == (this.arr_length-1)) {
+        if(confirm("END OF PAGE. Want to add new post")) {
+          const o: PostResponse = new PostResponse(this.post_id+1, "new page"+1, "test"+1, this.created_at, this.vote+1, this.communityIcon);
+          console.log("Add new post: "+o.post_id)
+          this.addNewPost(o);
+        }
+        else {
+          console.log("No add post")
+        }
+      }
+    }
+  }
+
+  addNewPost(o: PostResponse) {
+    this.event.emit(o);
   }
 }
