0f71fa3dadbe4b98270d76619d671db8a4466bb8
trxyzng <trxyzng37@gmail.com>
2024-04-07 15:00:37 +0700

    add img-caption component

---
+++ src/app/test/components/img-caption/img-caption/img-caption.component.ts
@@ -0,0 +1,45 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Img } from 'src/app/test/img';
+
+@Component({
+  selector: 'app-img-caption',
+  templateUrl: './img-caption.component.html',
+  styleUrl: './img-caption.component.scss'
+})
+export class ImgCaptionComponent {
+
+  @Input() img: Img = new Img("");
+  @Output() imgOutput = new EventEmitter<Img>;
+
+  public form: any = new FormGroup({
+    caption: new FormControl(''),
+    link: new FormControl(''),
+  })
+
+
+
+  public characterCount: number = 0;
+
+  onTextAreaInput(event: any) {
+    const textareaEle: any = event.target;
+    textareaEle.value = textareaEle.value.replace(/(\r\n|\n|\r)/gm, "");
+    textareaEle.style.height = 'auto';
+    textareaEle.style.height = `${textareaEle.scrollHeight}px`;
+    this.characterCount = textareaEle.value.length;
+    if (textareaEle.value === "") {
+      textareaEle.style.height = '30px';
+      this.characterCount = 0;
+    }
+    this.onInputChange();
+  }
+
+  onInputChange() {
+    this.img.caption = this.form.value.caption;
+    this.img.link = this.form.value.link;
+    console.log("on input change child value: ");
+    console.log(this.img)
+    this.imgOutput.emit(this.img);
+  }
+
+}
