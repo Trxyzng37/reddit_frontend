b38f15c2953a7e919ee2fc8ce3f3c11831b733eb
trxyzng <trxyzng37@gmail.com>
2024-04-30 21:05:43 +0700

    add delete comment function

---
+++ src/app/comment/pojo/delete-comment-response.ts
@@ -0,0 +1,7 @@
+export class DeleteCommentResponse {
+    public deleted: boolean;
+
+    public constructor(deleted: boolean) {
+        this.deleted = deleted;
+    }
+}
---
+++ src/app/comment/service/delete-comment/delete-comment.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PostService } from 'src/app/shared/services/post/post.service';
+import { StorageService } from 'src/app/shared/storage/storage.service';
+import { DeleteCommentRequest } from '../../pojo/delete-comment-request';
+import { HttpHeaders } from '@angular/common/http';
+import { DeleteCommentResponse } from '../../pojo/delete-comment-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeleteCommentService {
+
+  constructor(
+    private postService: PostService,
+    private storageService: StorageService
+  ) { }
+
+  private endpoint: string = "/delete-comment";
+
+  deleteComment(post_id: number, _id: number): Observable<DeleteCommentResponse> {
+    const uid = parseInt(this.storageService.getItem("uid"));
+    const request = new DeleteCommentRequest(post_id, uid, _id);
+    const body: string = JSON.stringify(request);
+    let header: HttpHeaders = new HttpHeaders();
+    header = header.append("Accept", 'application/json');
+    header = header.append('Content-Type', 'application/json');
+    return this.postService.post<DeleteCommentResponse>(this.endpoint, header, body, false);
+  }
+}
