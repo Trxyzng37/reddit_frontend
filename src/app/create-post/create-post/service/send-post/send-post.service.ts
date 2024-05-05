4b221e4950e4a4802fc3f5977502282b0a587eb8
trxyzng <trxyzng37@gmail.com>
2024-05-05 13:25:40 +0700

    change post data type

--- src/app/create-post/create-post/service/send-post/send-post.service.ts
+++ src/app/create-post/create-post/service/send-post/send-post.service.ts
@@ -1,9 +1,10 @@
 import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { PostRequest } from 'src/app/create-post/pojo/create-post-request';
+import { CreatePostRequest } from 'src/app/create-post/pojo/create-post-request';
 import { CreatePostResponse } from 'src/app/create-post/pojo/create-post-response';
 import { PostService } from 'src/app/shared/services/post/post.service';
+import { StorageService } from 'src/app/shared/storage/storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,15 @@ import { PostService } from 'src/app/shared/services/post/post.service';
 export class SendPostService {
 
   constructor(
-    private postService: PostService
-
+    private postService: PostService,
+    private storageService: StorageService
   ) {}
 
   private endpoint: string = "/create-post";
 
-  createPost(type: string, username: string, community: string, title: string, content: string, created_at: Date): Observable<CreatePostResponse> {
-    const request = new PostRequest(type, username, community, title, content, created_at);
+  createPost(type: string, community_id: number, title: string, content: string, created_at: Date): Observable<CreatePostResponse> {
+    const uid = this.storageService.getItem("uid") === "" ? 0 :  Number.parseInt(this.storageService.getItem("uid"));
+    const request = new CreatePostRequest(type, uid, community_id, title, content, created_at);
     const body: string = JSON.stringify(request);
     // console.log(request)
     let header: HttpHeaders = new HttpHeaders();
--- src/app/post-link-list/pojo/get-post-response.ts
+++ src/app/post-link-list/pojo/get-post-response.ts
@@ -1,6 +1,7 @@
 export class GetPostResponse {
     public post_id: number;
     public type: string;
+    public uid: number;
     public username: string;
     public username_icon: string;
     public community_name: string;
@@ -9,10 +10,12 @@ export class GetPostResponse {
     public content: string;
     public created_at: string;
     public vote: number;
+    // public deleted: number;
 
-    public constructor(post_id: number, type: string, username: string, username_icon: string, community_name: string, community_icon: string, title: string, content: string, created_at: string, vote: number) {
+    public constructor(post_id: number, type: string, uid: number, username: string, username_icon: string, community_name: string, community_icon: string, title: string, content: string, created_at: string, vote: number) {
         this.post_id = post_id;
         this.type = type;
+        this.uid = uid;
         this.username = username;
         this.username_icon = username_icon;
         this.community_name = community_name;
@@ -21,5 +24,6 @@ export class GetPostResponse {
         this.content = content;
         this.created_at = created_at;
         this.vote = vote;
+        // this.deleted = deleted;
     }
 }
