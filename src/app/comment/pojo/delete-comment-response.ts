7fd66c1ec1acb0e86bfe1ab4a9844c3c7e8fef8a
trxyzng <trxyzng37@gmail.com>
2024-05-05 17:25:22 +0700

    add error property to delete-comment-response

--- src/app/comment/pojo/delete-comment-response.ts
+++ src/app/comment/pojo/delete-comment-response.ts
@@ -1,7 +1,9 @@
 export class DeleteCommentResponse {
     public deleted: boolean;
+    public error: string;
 
-    public constructor(deleted: boolean) {
+    public constructor(deleted: boolean,error: string) {
         this.deleted = deleted;
+        this.error = error;
     }
 }
