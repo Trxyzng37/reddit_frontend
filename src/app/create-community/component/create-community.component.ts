e21a6fc2c22559140741c5ac70e501451892880f
trxyzng <trxyzng37@gmail.com>
2024-05-08 00:00:11 +0700

    add create community function

---
+++ src/app/create-community/component/create-community.component.ts
@@ -0,0 +1,97 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import tinymce from 'tinymce';
+import { StorageService } from 'src/app/shared/storage/storage.service';
+import { CreateCommunityService } from '../service/create-community/create-community.service';
+import { CreateCommunityResponse } from '../pojo/create-community-response';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'create-community',
+  templateUrl: './create-community.component.html',
+  styleUrl: './create-community.component.scss'
+})
+export class CreateCommunityComponent {
+
+  public constructor(
+    private storageService: StorageService,
+    private createCommunityService: CreateCommunityService,
+    private route: Router
+  ) {}
+
+  public createCommunityForm: any = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(21), Validators.pattern("^[0-9a-zA-Z ~`!@#$%^&*()_+={[}]|\\:;\"'<,>.?/-]$")])
+  })
+
+  @Output() closeFormEvent = new EventEmitter<boolean>();
+  public name_status: string = 'INVALID'; 
+
+  public characterCount: number = 0;
+  public description: string = "";
+  public avatar_url: string = "../../../assets/icon/dashed_circle.png";
+  public allowSubmit: boolean = false;
+
+  ngOnInit() {
+    this.createCommunityForm.get('name').valueChanges.subscribe( () => {
+      this.name_status = this.createCommunityForm.get('name').status;
+      this.AllowSubmit();
+    });
+  }
+
+  AllowSubmit() {
+    this.allowSubmit = this.name_status == "VALID" && this.description.length >= 0 && this.avatar_url != "../../../assets/icon/dashed_circle.png";
+    console.log(this.allowSubmit)
+  }
+
+  closeForm() {
+    this.closeFormEvent.emit(false);
+  }
+
+  inputTitle(event: any) {
+    const textareaEle: any = event.target;
+    textareaEle.value = textareaEle.value.replace(/(\r\n|\n|\r)/gm, "");
+    textareaEle.style.height = 'auto';
+    textareaEle.style.height = `${textareaEle.scrollHeight}px`;
+    this.characterCount = textareaEle.value.length;
+    if (textareaEle.value === "") {
+      textareaEle.style.height = '48px';
+      this.characterCount = 0;
+    }
+    this.description = textareaEle.value;
+    this.AllowSubmit();
+  }
+
+  upLoadImg(event: any) {
+    const files: FileList = event.target.files;
+    const file = files[0];
+    this.onImageUpload(file);
+  }
+
+  onImageUpload(file: File) {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.addEventListener("loadend", () => {
+      const data = reader.result as string;
+      this.avatar_url = data;
+      this.AllowSubmit();
+    })
+  }
+
+  submit() {
+    const uid = this.storageService.getItem("uid") === "" ? 0 :  Number.parseInt(this.storageService.getItem("uid"));
+    this.createCommunityService.createCommunity(uid, this.createCommunityForm.value.name, this.description, this.avatar_url).subscribe({
+      next: (response: CreateCommunityResponse) => {
+        Swal.fire('Create community successfully', '', 'success').then((result) => {
+          if (result.isConfirmed)
+            this.route.navigate(["/community/"+response.community_id]);
+        })
+      },
+      error: (e: HttpErrorResponse) => {
+        console.log("HttpServletResponse: " + e.error.message + "\n" + "ResponseEntity: " + e.error);
+      }
+    })
+  }
+}
---
+++ src/app/create-community/pojo/create-community-response.ts
@@ -0,0 +1,7 @@
+export class CreateCommunityResponse {
+    public community_id: number;
+
+    public constructor(community_id: number) {
+        this.community_id = community_id;
+    }
+}
