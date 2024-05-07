185bc9d3bdd5dcbacba493ce38cd245589436d2d
trxyzng <trxyzng37@gmail.com>
2024-05-08 00:00:41 +0700

    add create community button to navigation bar

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -9,16 +9,15 @@ import { AccessTokenService } from '../../../shared/services/access-token/access
 })
 
 export class HomeComponent implements OnInit {
-  private access_token: string;
   public recent_status: string = 'down';
   public favorite_status: string = 'down';
+  public isCreateCommunityFormShow: boolean = false;
 
   constructor( 
-    private accessTokenService: AccessTokenService, 
-    private http: HttpClient,
-    private elementRef: ElementRef<HTMLElement>
-  ) {
-    this.access_token = "";
+  ) {}
+
+  ngOnInit(): void {
+  
   }
 
   change_recent_status() {
@@ -31,30 +30,9 @@ export class HomeComponent implements OnInit {
     console.log(this.favorite_status)
   }
 
-  ngOnInit(): void {
-    // this.accessTokenService.get_access_token_from_server().subscribe({
-    //   next: (response) => {
-    //     this.accessTokenService.set_access_token(response);
-    //     this.access_token = this.accessTokenService.get_access_token();
-    //     alert("Get access_token from server: " + this.access_token);
-    //   },
-    //   error: (err) => {
-    //     console.log("Access token error:")
-    //     console.log(err)
-    //     alert("No access token")
-    //   }
-    // })
-  }
 
-  // send() {
-  //   let header: HttpHeaders = this.accessTokenService.set_access_token_header();
-  //   this.http.get("http://127.0.0.1:8080/check-access-token", {headers: header, observe: 'body', responseType: "text", withCredentials: true}).subscribe({
-  //     next: (response) => {
-  //       alert(response);
-  //     },
-  //     error: (e) => {
-  //       alert("Error\n"+e);
-  //     }
-  //   })
-  // }
+
+  openCreateCommunityForm() {
+    this.isCreateCommunityFormShow = !this.isCreateCommunityFormShow;
+  }
 }
