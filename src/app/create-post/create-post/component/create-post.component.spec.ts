01a667859bacfec66ccdef13249343710dda876a
trxyzng <trxyzng37@gmail.com>
2024-05-09 20:32:28 +0700

    change component name

--- src/app/create-post/create-post/component/create-post.component.spec.ts
+++ src/app/create-post/create-post/component/create-post.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TestComponent } from './create-post.component';
+import { CreatePostComponent } from './create-post.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('TestComponent', () => {
-  let component: TestComponent;
-  let fixture: ComponentFixture<TestComponent>;
+  let component: CreatePostComponent;
+  let fixture: ComponentFixture<CreatePostComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TestComponent],
+      imports: [CreatePostComponent],
       // schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(TestComponent);
+    fixture = TestBed.createComponent(CreatePostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
