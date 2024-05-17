edd0f88e6ba5dbd10d5cc6d774057c5717464679
trxyzng <trxyzng37@gmail.com>
2024-05-17 20:30:14 +0700

    add recent visit community in navigation bar

--- src/app/shared/pojo/default-response.ts
+++ src/app/shared/pojo/default-response.ts
@@ -1,7 +1,9 @@
 export class DefaultResponse {
     public error_code: number;
+    public error_description: string;
 
-    public constructor(error_code: number) {
+    public constructor(error_code: number, error_description: string) {
         this.error_code = error_code;
+        this.error_description = error_description;
     }
 }
