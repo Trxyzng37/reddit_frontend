719638377686250575adfb87ff71ccd238b36244
trxyzng <trxyzng37@gmail.com>
2024-05-27 23:03:52 +0700

    change cahnge password to use email parameter

--- src/app/change-password/services/change-password.service.ts
+++ src/app/change-password/services/change-password.service.ts
@@ -5,6 +5,7 @@ import { ChangePasswordResponse } from '../pojo/change-password-response';
 import { Observable } from 'rxjs';
 import { ChangePasswordRequest } from '../pojo/change-password-request';
 import { StorageService } from '../../../app/shared/storage/storage.service';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
@@ -19,10 +20,7 @@ export class ChangePasswordService {
 
   private endpoint: string = "/change-password"
 
-  public changePassword(newPassword: string): Observable<ChangePasswordResponse> {
-    const email: string = this.storageService.getItem("forgot-password-email");
-    if (email === "")
-      alert("Empty email for change password");
+  public changePassword(email: string, newPassword: string): Observable<ChangePasswordResponse> {
     const changePasswordRequest: ChangePasswordRequest = new ChangePasswordRequest(email, newPassword);
     const requestBody = JSON.stringify(changePasswordRequest);
     const header: HttpHeaders = new HttpHeaders();
