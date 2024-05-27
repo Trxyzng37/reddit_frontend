03ebd566ef53c02b013f9f78ba70437b7e07832f
trxyzng <trxyzng37@gmail.com>
2024-05-27 20:42:21 +0700

    change signin to not use refreshtoken

--- src/app/signin/pojo/username-password-signin-response.ts
+++ src/app/signin/pojo/username-password-signin-response.ts
@@ -1,9 +1,11 @@
 export class UsernamePasswordSignInResponse {
     public isSignIn: boolean;
     public passwordError: boolean;
+    public uid: number;
 
-    public constructor(isSignIn: boolean, passwordError: boolean) {
+    public constructor(isSignIn: boolean, passwordError: boolean, uid: number) {
         this.isSignIn = isSignIn;
         this.passwordError = passwordError;
+        this.uid = uid;
     }
 }
