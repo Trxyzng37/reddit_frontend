fcaf082b9e92a5d190a2d9acc8dc35c28ca21e65
trxyzng <trxyzng37@gmail.com>
2024-06-04 23:18:54 +0700

    add open navigation button when screen size smaller than 900px

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { AccessTokenService } from '../../../shared/services/access-token/access-token.service';
 
 @Component({
@@ -12,13 +12,12 @@ export class HomeComponent implements OnInit {
   public recent_status: string = 'down';
   public favorite_status: string = 'down';
   public isCreateCommunityFormShow: boolean = false;
+  isNavigationOpen: boolean = true;
 
   constructor( 
   ) {}
 
-  ngOnInit(): void {
-  
-  }
+  ngOnInit(): void {}
 
   change_recent_status() {
     this.recent_status = this.recent_status === 'up' ? 'down':'up';
@@ -30,9 +29,22 @@ export class HomeComponent implements OnInit {
     console.log(this.favorite_status)
   }
 
-
-
   openCreateCommunityForm() {
     this.isCreateCommunityFormShow = !this.isCreateCommunityFormShow;
+  }
+
+  openNavigation(event: any) {
+    this.isNavigationOpen = event.data;
+    // alert(this.isNavigationOpen)
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if(event.target.innerWidth > 900) {
+        this.isNavigationOpen = true;
+      }
+    else {
+      this.isNavigationOpen = false;
+    } 
   }
 }
