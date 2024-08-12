228cf7269002c6ef7260eda2acfc1de7ba4af6a4
trxyzng <trxyzng37@gmail.com>
2024-08-13 00:14:31 +0700

    change link view when in view post page

--- src/app/post-link/link-review/component/link-view/link-view.component.ts
+++ src/app/post-link/link-review/component/link-view/link-view.component.ts
@@ -3,6 +3,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { GetOpenGraphService } from '../../service/getOpenGraph/get-open-graph.service';
 import { OpenGraphResponse } from '../../pojo/open-graph-response';
 import { HttpErrorResponse } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 // import { parseFromDocument } from 'parse-open-graph';
 @Component({
   selector: 'app-link-view',
@@ -12,33 +13,31 @@ import { HttpErrorResponse } from '@angular/common/http';
 export class LinkViewComponent {
 
   public constructor(
-    public sanitizer: DomSanitizer,
-    private openGraphService: GetOpenGraphService
   ) {}
 
   @Input() title: string = "";
   @Input() content: string = "";
   
-  public isYoutubeLink: boolean = false;
   public data!: OpenGraphResponse;
-  regex = /https:\/\/www\.youtube\.com\/watch\?v=([^]+)/;
+  public link_domain: string = "";
+
+  isViewPostPage = window.location.href.match("/post/");
   
   ngOnInit() {
     this.data = JSON.parse(this.content);
-    // alert(this.data.link)
-    // if (this.data?.link.match(this.regex)) {
-    //   this.isYoutubeLink = true;
-    //   const videoId = this.data.link.match(this.regex)
-    //   this.data.link = `https://www.youtube.com/embed/${videoId![1]}`;
-    // }
-    // else {
-      // if(this.data.image == "")
-      //   this.data.image = "../../../../../assets/icon/logo.png";
-    // }
+    let regex = /^(https?:\/\/[^\/]+)/;
+    const found = this.data.link.match(regex);
+    if(found != null && found != undefined) {
+      this.link_domain = found[0];
+    }
   }
 
   preventClick(event: Event) {
     event.stopPropagation();
+  }
+
+  openLink() {
+    window.open(this.data.link, "_blank");
   }
 }
 
