665bdb13d4206a91af020c51d66886ce19fcb040
trxyzng <trxyzng37@gmail.com>
2024-09-15 21:45:24 +0700

    change to use routerLink

--- src/app/navigation_bar/component/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation_bar/component/navigation-bar/navigation-bar.component.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { JoinCommunity } from 'src/app/shared/pojo/join-community';
 import { Communities } from 'src/app/shared/pojo/pojo/communities';
 import { DarkModeService } from 'src/app/shared/services/dark-mode/dark-mode.service';
 import { RecentVisitService } from 'src/app/shared/services/recent-visit/recent-visit.service';
 import { CommunityService } from 'src/app/shared/services/search-communites/community.service';
+import { ShareDataService } from 'src/app/shared/services/share_data/share-data.service';
 import { StorageService } from 'src/app/shared/storage/storage.service';
 
 @Component({
@@ -17,7 +19,9 @@ export class NavigationBarComponent {
     private communityService: CommunityService,
     private storageService: StorageService,
     private recentVisitService: RecentVisitService,
-    private darkmodeSerive: DarkModeService
+    private darkmodeSerive: DarkModeService,
+    private shareDataService: ShareDataService,
+    private route: Router
   ) {}
 
   public isOpen: boolean = true;
@@ -26,18 +30,25 @@ export class NavigationBarComponent {
   public favorite_status: string = 'down';
   public moderation_status:boolean = false;
 
+
   @Output() showCommunityFormEvent = new EventEmitter<boolean>();
 
   public favoriteCommunities: Communities[] = [];
   public recentVisitCommunities: Communities[] = [];
   public moderationCommunities: Communities[] = [];
 
+  public isHomePage: boolean = window.location.href.includes("/home");
+  public isPopularPage: boolean = window.location.href.includes("/popular");
+
   ngOnInit() {
     this.darkmodeSerive.useDarkMode();
+    this.shareDataService.subscribed_communities$.subscribe(res => {
+      this.favoriteCommunities = res;
+    })
     const uid = this.storageService.getItem("uid") == "" ? 0 : Number.parseInt(this.storageService.getItem("uid"));
     this.isUser = uid != 0;
     if(uid != 0) {
-      this.communityService.getCommunityInfoByUid(uid).subscribe({
+      this.communityService.getCommunityModerateByUid(uid).subscribe({
         next: (response: Communities[]) => {
           this.moderationCommunities = response;
         }
@@ -47,17 +58,14 @@ export class NavigationBarComponent {
           this.recentVisitCommunities = response;
         }
       })
-      this.storageService.removeItem("join_community");
-      this.communityService.getSubscribedCommunitiesByUid(uid).subscribe({
-        next: (response: Communities[]) => {
-          this.favoriteCommunities = response;
-          let join_community_arr: JoinCommunity[] = [];
-          for(let community of response) {
-            join_community_arr.push(new JoinCommunity(community.id, true));
+      if(this.favoriteCommunities.length == 0) {
+        this.communityService.getSubscribedCommunitiesByUid(uid).subscribe({
+          next: (response: Communities[]) => {
+            this.favoriteCommunities = response;
+            this.shareDataService.setSubscribedCommunities(this.favoriteCommunities);
           }
-          this.storageService.setItem("join_community", JSON.stringify(join_community_arr));
-        }
-      })
+        })
+      }
     }
     else {
       let recent_communities_array: number[] = this.storageService.getItem("recent_communities") == "" ? [] : JSON.parse("[" + this.storageService.getItem("recent_communities") + "]");
@@ -69,6 +77,20 @@ export class NavigationBarComponent {
         })
       }
     }
+    setInterval(()=>{
+      if(window.location.href.includes("/home")) {
+        this.isHomePage = true;
+        this.isPopularPage = false;
+      }
+      else if(window.location.href.includes("/popular")) {
+        this.isPopularPage = true;
+        this.isHomePage = false;
+      }
+      else {
+        this.isHomePage = false;
+        this.isPopularPage = false;
+      }
+    }, 100);
   }
 
   ngAfterViewInit() {
@@ -90,5 +112,19 @@ export class NavigationBarComponent {
 
   showCreateCommunityForm() {
     this.showCommunityFormEvent.emit(true);
+  }
+
+  goToHome() {
+    this.shareDataService.setHomeCurViewPostId(0);
+    this.shareDataService.setHomeSearchOption("hot");
+    this.shareDataService.setPopularDetailPosts([]);
+    this.shareDataService.setPopularPostIdArr([]);
+  }
+
+  goToPopular() {
+    this.shareDataService.setPopularCurViewPostId(0);
+    this.shareDataService.setPopularSearchOption("hot");
+    this.shareDataService.setHomeDetailPosts([]);
+    this.shareDataService.setHomePostIdArr([]);
   }
 }
